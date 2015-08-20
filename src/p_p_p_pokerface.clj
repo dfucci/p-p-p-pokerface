3f70a0f18b50195b8c03aa0ed79955a112f5091a
dfucci <davfuc@gmail.com>
2015-08-21 01:42:02 +0300

    finished

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -9,7 +9,6 @@
   (let [[_ s] card]
     (str s)))
 
-
 (defn pair? [hand]
   (= 2 (apply max (vals (frequencies (map rank hand))))))
 
@@ -29,10 +28,25 @@
   (= '(2 2) (take 2 (vals (frequencies (map rank hand))))))
 
 (defn straight? [hand]
-  nil)
+  (let [ace-high (sort (map rank hand))
+        ace-low (sort (replace {14 1} (map rank hand)))]
+    (or (= ace-high (range (first ace-high) (inc (last ace-high))))
+        (= ace-low (range (first ace-low) (inc (last ace-low)))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(defn high-card?  [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (let  [checkers #{[high-card? 0]  [pair? 1]
+                    [two-pairs? 2]  [three-of-a-kind? 3]
+                    [straight? 4]   [flush? 5]
+                    [full-house? 6]  [four-of-a-kind? 7]
+                    [straight-flush? 8]}
+         passing-checks (filter (fn [c] ((first c) hand)) checkers)]
+    (apply max (map second passing-checks))))
+
+
+
